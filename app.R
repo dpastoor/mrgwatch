1e819b94e3e49f4d6c42c53e34254153fc427080
Devin Pastoor <devin.pastoor@gmail.com>
2017-01-12 14:17:34 -0500

    render table of file info polled

--- app.R
+++ app.R
@@ -1,10 +1,11 @@
 library(shiny)
 library(miniUI)
-
+library(dplyr)
 ui <- miniPage(
   gadgetTitleBar("watch mrgsolve models"),
-  miniContentPanel(padding = 0,
-                   tableOutput("modelStatus")
+  miniContentPanel(
+                   DT::dataTableOutput('modelStatus'),
+                   height = "100%"
   ),
   miniButtonBlock(
     actionButton("reset", "Reset")
@@ -12,17 +13,31 @@ ui <- miniPage(
 )
 
 server <- function(input, output, session) {
-  rv <- reactiveValues(
-    tbl = data.frame(model = c("mod1", "mod2"), mod_time = c(as.numeric(Sys.time()) %% 86000, as.numeric(Sys.time()) %% 86000))
-  )
-  output$modelStatus <- renderTable(rv$tbl)
+  data_available <- reactivePoll(2000, session, 
+                                 checkFunc = function() {
+                                   file.info(list.files(pattern = "*.cpp")) %>%
+                                     select(-atime)
+                                 },
+                                 valueFunc = function() {
+                                   file.info(list.files(pattern = "*.cpp")) %>%
+                                     select(mtime)
+                                 })
+  # rv <- reactiveValues(
+  #   tbl = data.frame(model = c("mod1", "mod2"), mod_time = c(as.numeric(Sys.time()) %% 86000, as.numeric(Sys.time()) %% 86000))
+  # )
+  output$modelStatus <- DT::renderDataTable({
+    data_available() %>% mutate(model = row.names(.),
+                                working = TRUE) %>%
+      select(model, everything()) 
+  }, class = "cell-border stripe compact", 
+  options = list(pageLength = 10, dom = 'tip'))
+  
   observeEvent(input$done, {
     stopApp(TRUE)
   })
   
   observeEvent(input$reset, {
-    print('reset')
-    rv$tbl <- data.frame(model = c("mod1", "mod2"), mod_time = c(as.numeric(Sys.time()) %% 86000, as.numeric(Sys.time()) %% 86000))
+    TRUE
   })
 }
 
